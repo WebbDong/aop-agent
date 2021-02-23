01482d8644c9ca9a2ce5b9be5371a87a95b4831e
Webb Dong <wangwebb1991@gmail.com>
2021-02-23 20:54:54 +0800

    Dog

---
+++ aop-agent-test/src/main/java/com/webbdong/aop/agent/test/bean/Dog.java
@@ -0,0 +1,14 @@
+package com.webbdong.aop.agent.test.bean;
+
+/**
+ * Dog
+ * @author Webb Dong
+ * @date 2021-02-21 15:38
+ */
+public class Dog {
+
+    public void run() {
+        System.out.println("Target method Dog.run()");
+    }
+
+}
